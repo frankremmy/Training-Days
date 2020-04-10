34de3ee0678098f67e078f91a9599e04de208fe2
Ugochukwu Remigius Chigbata <princeugo.remix@gmail.com>
2020-04-10 17:14:01 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,51 @@
+// Generates a random number 0-2 
+const getRandEvent = () => {
+    const random = Math.floor(Math.random() * 3);
+    if (random === 0) {
+      return 'Marathon';
+    } else if (random === 1) {
+      return 'Triathlon';
+    } else if (random === 2) {
+      return 'Pentathlon';
+    }
+  };
+
+// Days variable is replaced based on the getRandEvent output
+const getTrainingDays = event => {
+    let days;
+    if (event === 'Marathon') {
+        days = 50;
+    } else if (event === 'Triathlon') {
+        days = 100;
+    } else if (event === 'Pentathlon') {
+        days = 200;
+    }
+    return days;
+};
+
+// Basic functions to console log our functions from earlier
+const logEvent = (event, name) => {
+console.log(`${name}'s event is: ${event}`);
+};
+
+const logTime = (days, name) => {
+console.log(`${name}'s time to train is: ${days} days`);
+};
+
+// Variables
+const event = getRandEvent();
+const days = getTrainingDays(event);
+const name = 'Nala';
+
+// Output
+logEvent(event, name);
+logTime(days, name);
+
+// Variables -- Test 2
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+const name2 = 'Warren';
+
+// Output -- Test 2
+logEvent(event2, name2);
+logTime(days2, name2);
